343bf00ee69e6e3f4332705ddcd88e5d2e2f99d3
NAVYA GOLLAPALLI <gollapallinavya1@gmail.com>
2023-06-28 12:30:45 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee/employee-list.component';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    FilterPipe,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
